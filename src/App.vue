d4473a507b4dc1dd3232247460e33d4cac281915
RoHell <karol-roch@wp.pl>
2018-08-11 19:12:40 +0200

    sass

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,11 @@
     router-view
 </template>
 
-<style>
-#app {
-  font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: 'Roboto', sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  margin-top: 60px
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -2,3 +2,12 @@
   .about
     h1 This is an about page
 </template>
+
+<script>
+</script>
+
+<style lang="sass" scoped>
+.about
+  color: red
+</style>
+
